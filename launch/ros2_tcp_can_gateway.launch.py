bd8e94c3ade56494a2a4cfdd7439b6f2d6295584
Victor Belov <belovictor@gmail.com>
2025-01-20 02:26:25 +0300

    Add launch arguments

--- launch/ros2_tcp_can_gateway.launch.py
+++ launch/ros2_tcp_can_gateway.launch.py
@@ -2,11 +2,27 @@ import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'can_interface',
+            default_value='vcan0',
+            description='Default can interface name'
+        ),
+        DeclareLaunchArgument(
+            'gateway_host',
+            default_value='192.168.1.4',
+            description='Default can gateway host'
+        ),
+        DeclareLaunchArgument(
+            'gateway_port',
+            default_value='20001',
+            description='Default can gateeeway port'
+        ),
         Node(
             output='screen',
             emulate_tty=True,
@@ -14,9 +30,9 @@ def generate_launch_description():
             executable='ros2_tcp_can_gateway_node',
             name='ros2_tcp_can_gateway_node',
             parameters=[
-                {'can_interface': 'vcan0'},
-                {'gateway_host': '192.168.1.4'},
-                {'gateway_port': 20001},
+                {'can_interface': LaunchConfiguration('can_interface')},
+                {'gateway_host': LaunchConfiguration('gateway_host')},
+                {'gateway_port': LaunchConfiguration('gateway_port')},
             ]
         ),
     ])
